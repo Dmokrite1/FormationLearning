3753f372e28eb0ace2b0adf7946e1c4d730f513e
Dmokrite1 <hg_greg@proton.me>
2023-10-17 14:23:49 +0200

    add azertype

---
+++ Azertype/scripts/config.js
@@ -0,0 +1,6 @@
+/*
+* tableaux contenant les listes de phrases et de mots
+*/ 
+const listeMots = ["Cachalot", "Pétunia", "Serviette", "Aéronef", "Hippopotame", "Tubercule", "Funambule"]
+const listePhrases = ["Un mathématicien n'urine pas. Il fait π π", "Pour la Horde !", "Pas de panique !", "La vie, l'univers et tout le reste",
+"Merci pour le poisson", "Attention la tête", "Félicitations pour votre réussite", "En avant toute"] 
