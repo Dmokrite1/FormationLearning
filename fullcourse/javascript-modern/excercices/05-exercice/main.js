df676f86b70cb497b6e28c86d42a2e53fdfe9453
Dmokrite1 <hg_greg@proton.me>
2023-11-10 15:14:45 +0100

    modify dossier name for better convention

--- fullcourse/javascript-modern/excercices/05-exercice/main.js
+++ fullcourse/javascript-modern/excercices/05-exercice/main.js
@@ -1,14 +1,16 @@
+/*
 const todoListElement = document.getElementById('todoList');
 fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json())
     .then(todos => {
         console.log('https://jsonplaceholder.typicode.com/todos', todos)
-        todos.map(todo => {
+        //todos.map(todo => {
             const listItem = document.createElement('li');
             listItem.innerHTML = `${todo.userId} - ${todo.id} - ${todo.title} - ${todo.completed === true? 'fait' : 'pas fait'}`;
-            todoListElement.appendChild(listItem);
+            //todoListElement.appendChild(listItem);
         });
     })
+*/
 
     /*
     *version corrigé
@@ -19,3 +21,16 @@ fetch('https://jsonplaceholder.typicode.com/todos')
     .then(todos => list.innerHTML = todos.join(''))
     .catch(err => console.error(err));
     */
+
+    /*
+    *version élégante avec async
+    */
+const list = document.querySelector("ul")
+async function getTodos(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await response.json()
+    const todo = data.map(t => `<li>${t.userId} - ${t.id} - ${t.title} - ${t.completed? "fait" : "pas fait"}</li>`)
+    list.innerHTML = todo.join("")
+}
+
+getTodos()
