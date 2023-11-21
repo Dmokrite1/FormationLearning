0740d7e9216dcce09d3303f29101e16306820783
Dmokrite1 <hg_greg@proton.me>
2023-11-21 11:00:14 +0100

    starting course localStorage

---
+++ fullcourse/javascript-modern/22-local-storage/assets/js/listOfBooks.js
@@ -0,0 +1,5 @@
+import List from "./components/list.js"
+import Header from "./components/header.js"
+
+customElements.define("main-list", List)
+customElements.define("main-header", Header)
---
+++ fullcourse/javascript-modern/22-local-storage/assets/js/main.js
@@ -0,0 +1,5 @@
+import Accueil from "./components/accueil.js"
+import Header from "./components/header.js"
+
+customElements.define("main-home", Accueil)
+customElements.define("main-header", Header)
