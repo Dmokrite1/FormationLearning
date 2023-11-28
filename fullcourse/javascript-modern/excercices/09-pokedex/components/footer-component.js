baa40cf7d32db720ec898e96a7a04ad85c31105f
Dmokrite1 <hg_greg@proton.me>
2023-11-28 14:55:09 +0100

    modify web-component footer (connectedCallback)

--- fullcourse/javascript-modern/excercices/09-pokedex/components/footer-component.js
+++ fullcourse/javascript-modern/excercices/09-pokedex/components/footer-component.js
@@ -4,43 +4,45 @@ class FooterComponent extends HTMLElement {
 
         // Créez un Shadow DOM
         this.attachShadow({ mode: 'open' });
+    }
 
-        // Définissez le contenu HTML et CSS du composant
-        this.shadowRoot.innerHTML = `
-            <style>
-                div {
-                    /* Styles pour la première div */
-                }
-
-                img {
-                    height: 50px;
-                }
-
-                .user-cards {
-                    display: flex;
-                    margin-left: 200px;
-                }
-                  
-                .user-card {
-                    margin: 0 10px;
-                    padding: 10px;
-                    border: 1px solid #ccc;
-                    border-radius: 5px;
-                    text-align: center;
-                    background-color: #fff;
-                    color: #333;
-                    margin-left: 250px;
-                }
-            </style>
-            <div>
-                <img src="./img/pok.jpg" alt="Logo">
-            </div>
-            <div class="user-cards">
-                <div class="user-card">User 1</div>
-                <div class="user-card">User 2</div>
-                <div class="user-card">User 3</div>
-            </div>
-        `;
+    connectedCallback(){
+                // Définissez le contenu HTML et CSS du composant
+                this.shadowRoot.innerHTML = `
+                <style>
+                    div {
+                        /* Styles pour la première div */
+                    }
+    
+                    img {
+                        height: 50px;
+                    }
+    
+                    .user-cards {
+                        display: flex;
+                        margin-left: 200px;
+                    }
+                      
+                    .user-card {
+                        margin: 0 10px;
+                        padding: 10px;
+                        border: 1px solid #ccc;
+                        border-radius: 5px;
+                        text-align: center;
+                        background-color: #fff;
+                        color: #333;
+                        margin-left: 250px;
+                    }
+                </style>
+                <div>
+                    <img src="./img/pok.jpg" alt="Logo">
+                </div>
+                <div class="user-cards">
+                    <div class="user-card">User 1</div>
+                    <div class="user-card">User 2</div>
+                    <div class="user-card">User 3</div>
+                </div>
+            `;
     }
 }
 
