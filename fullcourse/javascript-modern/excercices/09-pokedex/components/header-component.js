a17e1ae0f26448ab0c18709e8cc3784d31d7d381
Dmokrite1 <hg_greg@proton.me>
2023-11-28 16:01:23 +0100

    page active add

--- fullcourse/javascript-modern/excercices/09-pokedex/components/header-component.js
+++ fullcourse/javascript-modern/excercices/09-pokedex/components/header-component.js
@@ -1,16 +1,17 @@
 class HeaderComponent extends HTMLElement {
   connectedCallback() {
-
     this.innerHTML = this.getTemplate();
+    this.setupListeners();
+    this.highlightActiveLink();
   }
 
   getTemplate() {
     return `
       <header>
         <nav>
-          <a href="index.html">Monde</a>
-          <a href="pokedex.html">Pokedex</a>
-          <a href="historique.html">Historique</a>
+          <a href="index.html" id="link-monde">Monde</a>
+          <a href="pokedex.html" id="link-pokedex">Pokedex</a>
+          <a href="historique.html" id="link-historique">Historique</a>
         </nav>
         <div id="counter-container">
           <p>Attrapés: <span id="caught-count">0</span></p>
@@ -18,6 +19,33 @@ class HeaderComponent extends HTMLElement {
         </div>
       </header>
     `;
+  }
+
+  setupListeners() {
+    const links = this.querySelectorAll('a');
+
+    links.forEach(link => {
+      link.addEventListener('click', () => {
+        // Retirez la classe "active" de tous les liens
+        links.forEach(l => l.classList.remove('active'));
+
+        // Ajoutez la classe "active" au lien cliqué
+        link.classList.add('active');
+
+        // Enregistrez l'ID du lien actif dans le stockage local
+        localStorage.setItem('activeLinkId', link.id);
+      });
+    });
+  }
+
+  highlightActiveLink() {
+    const activeLinkId = localStorage.getItem('activeLinkId');
+    if (activeLinkId) {
+      const activeLink = this.querySelector(`#${activeLinkId}`);
+      if (activeLink) {
+        activeLink.classList.add('active');
+      }
+    }
   }
 }
 
