966c3f9db86c5bb30866110b04292bd2d4f94ed6
Dmokrite1 <hg_greg@proton.me>
2023-11-30 14:00:37 +0100

    add song pokemon operationnelle

--- fullcourse/javascript-modern/excercices/09-pokedex/components/footer-component.js
+++ fullcourse/javascript-modern/excercices/09-pokedex/components/footer-component.js
@@ -9,31 +9,6 @@ class FooterComponent extends HTMLElement {
     connectedCallback(){
                 // Définissez le contenu HTML et CSS du composant
                 this.shadowRoot.innerHTML = `
-                <style>
-                    div {
-                        /* Styles pour la première div */
-                    }
-    
-                    img {
-                        height: 50px;
-                    }
-    
-                    .user-cards {
-                        display: flex;
-                        margin-left: 200px;
-                    }
-                      
-                    .user-card {
-                        margin: 0 10px;
-                        padding: 10px;
-                        border: 1px solid #ccc;
-                        border-radius: 5px;
-                        text-align: center;
-                        background-color: #fff;
-                        color: #333;
-                        margin-left: 250px;
-                    }
-                </style>
                 <div>
                     <img src="./img/pok.jpg" alt="Logo">
                 </div>
