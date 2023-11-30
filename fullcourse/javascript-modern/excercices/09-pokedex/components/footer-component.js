8f32114398987d0e4406746a452a3976d9fea53e
Dmokrite1 <hg_greg@proton.me>
2023-11-30 14:43:25 +0100

    correction footer

--- fullcourse/javascript-modern/excercices/09-pokedex/components/footer-component.js
+++ fullcourse/javascript-modern/excercices/09-pokedex/components/footer-component.js
@@ -1,23 +1,6 @@
 class FooterComponent extends HTMLElement {
     constructor() {
         super();
-
-        // Créez un Shadow DOM
-        this.attachShadow({ mode: 'open' });
-    }
-
-    connectedCallback(){
-                // Définissez le contenu HTML et CSS du composant
-                this.shadowRoot.innerHTML = `
-                <div>
-                    <img src="./img/pok.jpg" alt="Logo">
-                </div>
-                <div class="user-cards">
-                    <div class="user-card">User 1</div>
-                    <div class="user-card">User 2</div>
-                    <div class="user-card">User 3</div>
-                </div>
-            `;
     }
 }
 
